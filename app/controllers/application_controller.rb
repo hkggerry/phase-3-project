f0081d07b20af940d7a92b2528cb4ae1414c4f08
gerry <hkggerry@gmail.com>
2022-06-10 16:55:17 -0700

    api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,15 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  
+  get "/calenders" do
+    calender = Calender.all
+    calender.to_json
+  end
+
+  get "/calenders/:id" do
+    calender = Calender.find(params[:id])
+    calender.to_json(include: :activities)
+  end
 
 end
