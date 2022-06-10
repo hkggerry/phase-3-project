5a55d2b78079035abb03a0017d07d4203c2e6a0c
gerry <hkggerry@gmail.com>
2022-06-10 16:46:12 -0700

    good?

--- db/migrate/20220610221318_create_activities.rb
+++ db/migrate/20220610221318_create_activities.rb
@@ -1,7 +1,6 @@
 class CreateActivities < ActiveRecord::Migration[6.1]
   def change
     create_table :activities do |t|
-      t.string :to_do
       t.string :location
       t.string :duration
     end
