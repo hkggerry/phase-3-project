b2c56ad146e6230bd04f50771e1e97f04cfd6751
gerry <hkggerry@gmail.com>
2022-06-10 15:42:48 -0700

    Getting there

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Calender.create(day: "Monday")
+Calender.create(day: "Tuesday")
+Calender.create(day: "Wednesday")
+Calender.create(day: "Thursday")
+Calender.create(day: "Friday")
+Calender.create(day: "Saturday")
+Calender.create(day: "Sunday")
+
+Activity.create(to_do: "Work", location: "Office", duration: "8 Hours")
+Activity.create(to_do: "Workout", location: "Gym", duration: "1.5 Hours")
+Activity.create(to_do: "Board Game Night", location: "Home", duration: "4 Hours")
+Activity.create(to_do: "Study", location: "Library", duration: "2 Hours")
+
 
 puts "✅ Done seeding!"
